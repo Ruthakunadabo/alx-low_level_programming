07035295abe5c822293bb536a2456fcbfcd01055
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-11 10:08:58 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point of C program
+ *
+ *   * Return:0
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
