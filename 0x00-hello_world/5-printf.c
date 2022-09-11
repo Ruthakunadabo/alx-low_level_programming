159703e0e02acef6da557a0b254e1e978233dbe9
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-11 10:58:18 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point of C program
- *
- *   * Return:0
- *
- *    */
-
-
+ * main - Entry point of C program
+ * Return:0
+ */
 
 int main(void)
-
 {
-
-
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
