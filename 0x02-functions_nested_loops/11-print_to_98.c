c195bc76a4e35668e5a253d69821cb05568be659
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-13 17:12:27 -0700

    Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,85 +1,41 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
 
 #define STOP 98
 
-
-
 /**
+ * print_to_98 - Print from n to 98.
  *
- *  * print_to_98 - Print from n to 98.
+ *  @n: function parameters. n is the starting
+ *  point of the printing.
  *
- *   *
- *
- *    * @n: function parameters. n is the starting
- *
- *     * point of the printing.
- *
- *      *
- *
- *       * Return: void
- *
- *        */
-
-
+ *   Return: void
+ */
 
 void print_to_98(int n)
-
 {
+int i;
 
-		int i;
-
-
-
-			if (n < STOP)
-
-					{
-
-								for (i = n; i <= STOP; i++)
-
-											{
-
-															if (i != STOP)
-
-																				printf("%d, ", i);
-
-																		else
-
-																							printf("%d", i);
-
-																				}
-
-									}
-
-				else if (n > STOP)
-
-						{
-
-									for (i = n; i >= STOP; i--)
-
-												{
-
-																if (i != STOP)
-
-																					printf("%d, ", i);
-
-																			else
-
-																								printf("%d", i);
-
-																					}
-
-										}
-
-					else
-
-								printf("%d", n);
-
-
-
-						printf("\n");
-
+if (n < STOP)
+{
+for (i = n; i <= STOP; i++)
+{
+if (i != STOP)
+printf("%d, ", i);
+else
+printf("%d", i);
+}
+}
+else if (n > STOP)
+{
+for (i = n; i >= STOP; i--)
+{
+if (i != STOP)
+printf("%d, ", i);
+else
+printf("%d", i);
+}
+}
+printf("%d", n);
+printf("\n");
 }
