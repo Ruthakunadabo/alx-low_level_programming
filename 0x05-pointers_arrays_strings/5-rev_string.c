c1330ae649f02b0c9926eabf6845e5db642311b7
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-19 15:50:46 -0700

    5-rev-=string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,53 @@
-u#include "main.h"
+#include "main.h"
+
+
 
 /**
- * rev_string - Reverse the given string.
  *
- * @s: string to be reversed.
+ *  * rev_string - Reverse the given string.
  *
- * Return: Nothing
- */
+ *   *
+ *
+ *    * @s: string to be reversed.
+ *
+ *     *
+ *
+ *      * Return: Nothing
+ *
+ *       */
+
+
 
 void rev_string(char *s)
+
 {
-int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
-;
-j = 0;
-for (i = i - 1; j < i; i--)
-{
-char temp = s[i];
+		int i, j;
 
-s[i] = s[j];
-s[j] = temp;
-j++;
 
-}
+
+			for (i = 0; s[i] != '\0'; i++)
+
+						;
+
+				j = 0;
+
+
+
+					for (i = i - 1; j < i; i--)
+
+							{
+
+										char temp = s[i];
+
+
+
+												s[i] = s[j];
+
+														s[j] = temp;
+
+																j++;
+
+																	}
+
 }
