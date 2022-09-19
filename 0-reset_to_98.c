6645f4b6408b9bb322fb871808d69d9bdfa41a6d
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-19 14:44:13 -0700

    0-reset_to_98.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * reset_to_98 - This function resets the value stored
+ *
+ *   * at he pointer to 98.
+ *
+ *    *
+ *
+ * * @n: The pointer to be updated.
+ *
+ * *
+ *
+ * Return: Nothing
+ *
+ *        */
+
+
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+
+}
