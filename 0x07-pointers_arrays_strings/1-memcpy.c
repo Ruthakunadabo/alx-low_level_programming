036d67175b0db87ac38d8fa48ca2db667dfc6634
Ruthakunadabo <ruthnaku96@gmail.com>
2022-09-27 13:39:12 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - A program the copies n bytes from src to dest
+ *
+ * @dest: memory to fill
+ * @src: memory to copy from
+ * @n: no of bytes to copy
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int m;
+
+for (m = 0; m < n; m++)
+dest[m] = src[m];
+
+return (dest);
+}
